83432da56f9d99e1516fbd2a4d965db343119e51
SuperXiang <12507538@qq.com>
2020-12-14 20:21:36 +0800

    modify documentation

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,24 @@
+.. PaddleHelix documentation master file, created by
+   sphinx-quickstart on Mon Dec 14 18:39:38 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to PaddleHelix's documentation!
+=======================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+   :glob:
+
+   *
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ pygments_style = 'sphinx'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0'
+release = 'V1.0.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -65,7 +65,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
