99b3084baf453649bc66ff761b6f8ec9d60d93f8
SuperXiang <12507538@qq.com>
2020-12-16 16:15:36 +0800

    modify docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ import sphinx_rtd_theme
 import os
 import sys
 import re
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
