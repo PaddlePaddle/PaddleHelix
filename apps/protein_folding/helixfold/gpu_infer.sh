635c5b86c837c39e5fd559e3301822a333c24cdb
GuoxiaWang <mingzilaochongtu@gmail.com>
2023-01-13 14:23:38 +0800

    fix

--- apps/protein_folding/helixfold/gpu_infer.sh
+++ apps/protein_folding/helixfold/gpu_infer.sh
@@ -13,27 +13,28 @@ OUTPUT_DIR="$root_path/demo_data/casp14_demo/output"
 log_dir="$root_path/demo_data/casp14_demo/demo_log"
 MODELS="model_1,model_5"
 SUBBATCH_SIZE=48
-DAP_DEGREE=1
 
-distributed=1
+distributed=true
 
 # 'fp32' or 'bf16'
 PRECISION='bf16'
 
-# enable dap or unified memory for EXTREMELY LONG SEQUENCE PROTEIN
-# DAP_DEGREE=8
-# export FLAGS_use_cuda_managed_memory=true
-
-# Reduce the size of subbatch_size when the gpu memory is not enough 
-# SUBBATCH_SIZE=1
-
-if [ $distributed == 1 ]
+if [ $distributed == true ]
 then
+  # enable dap for EXTREMELY LONG SEQUENCE PROTEIN
   python_cmd="python -m paddle.distributed.launch --log_dir=${log_dir} --gpus=0,1,2,3,4,5,6,7 "
   distributed_flag="--distributed"
+  DAP_DEGREE=8
+  # Reduce the size of subbatch_size when the gpu memory is not enough 
+  # SUBBATCH_SIZE=1
 else
+  # enable unified memory for EXTREMELY LONG SEQUENCE PROTEIN
+  # export FLAGS_use_cuda_managed_memory=true
   python_cmd="CUDA_VISIBLE_DEVICES=0 python "
   distributed_flag=""
+  DAP_DEGREE=1
+  # Reduce the size of subbatch_size when the gpu memory is not enough 
+  # SUBBATCH_SIZE=1
 fi
 
 $python_cmd \
